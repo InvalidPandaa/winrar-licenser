640b9d56017988160b95f3a953582e0ba5d15a99
InvalidPanda ツ <117936522+InvalidPandaa@users.noreply.github.com>
2022-11-13 20:25:39 +0100

    Initial v1.0 Release

--- WinRAR-Licenser/WinRAR-Licenser.bat
+++ WinRAR-Licenser/WinRAR-Licenser.bat
@@ -1,9 +1,5 @@
 @echo off
 
-REM -> The Variables for download url and path
-
-SET downloadURL = "https://raw.githubusercontent.com/InvalidPandaa/winrar-licenser/main/WinRAR%20Licenser/rarreg.key"
-SET downloadPath = "C:\Program Files\WinRAR\rarreg.key"
 
 title WinRAR-Licenser I v1.0 I by InvalidPandaa
 
@@ -33,9 +29,47 @@ echo.
 set /p optMain=Please select an option: 
 if '%optMain%' == '1' goto :install
 if '%optMain%' == '2' goto :remove
-if '%optMain%' == 'e' goto :exit
+if '%optMain%' == '3' goto :exit
+
+goto gui
 
 :install
+cls
+
+If exist "C:\Program Files\WinRAR\rarreg.key" (
+	echo You have already a licensed WinRAR version!
+	@ping -n 5 localhost> nul
+	goto gui
+)
+
+echo.
+echo.
+echo 		 _  _  __  __ _  ____   __   ____    __    __  ___  ____  __ _  ____  ____  ____ 
+echo 		/ )( \(  )(  ( \(  _ \ / _\ (  _ \  (  )  (  )/ __)(  __)(  ( \/ ___)(  __)(  _ \
+echo 		\ /\ / )( /    / )   //    \ )   /  / (_/\ )(( (__  ) _) /    /\___ \ ) _)  )   /
+echo 		(_/\_)(__)\_)__)(__\_)\_/\_/(__\_)  \____/(__)\___)(____)\_)__)(____/(____)(__\_)
+echo.
+echo.
+echo 		Downloading the required files...
+
+@ping -n 2 localhost> nul
+bitsadmin /transfer "Download Key" /download /priority normal https://raw.githubusercontent.com/InvalidPandaa/winrar-licenser/main/WinRAR-Licenser/rarreg.key "C:\Program Files\WinRAR\rarreg.key"
+cls
+@ping -n 1 localhost> nul
+
+echo.
+echo.
+echo 		 _  _  __  __ _  ____   __   ____    __    __  ___  ____  __ _  ____  ____  ____ 
+echo 		/ )( \(  )(  ( \(  _ \ / _\ (  _ \  (  )  (  )/ __)(  __)(  ( \/ ___)(  __)(  _ \
+echo 		\ /\ / )( /    / )   //    \ )   /  / (_/\ )(( (__  ) _) /    /\___ \ ) _)  )   /
+echo 		(_/\_)(__)\_)__)(__\_)\_/\_/(__\_)  \____/(__)\___)(____)\_)__)(____/(____)(__\_)
+echo.
+echo.
+echo 		The required files has been downloaded! WinRAR is now licensed!
+@ping -n 5 localhost> nul
+goto gui
+
+:remove
 cls
 
 echo.
@@ -46,9 +80,23 @@ echo 		\ /\ / )( /    / )   //    \ )   /  / (_/\ )(( (__  ) _) /    /\___ \ ) _
 echo 		(_/\_)(__)\_)__)(__\_)\_/\_/(__\_)  \____/(__)\___)(____)\_)__)(____/(____)(__\_)
 echo.
 echo.
-echo 	Downloading the required files...
+echo 		Removing the license...
+del /F "C:\Program Files\WinRAR\rarreg.key"
+	
+cls
+echo.
+echo.
+echo 		 _  _  __  __ _  ____   __   ____    __    __  ___  ____  __ _  ____  ____  ____ 
+echo 		/ )( \(  )(  ( \(  _ \ / _\ (  _ \  (  )  (  )/ __)(  __)(  ( \/ ___)(  __)(  _ \
+echo 		\ /\ / )( /    / )   //    \ )   /  / (_/\ )(( (__  ) _) /    /\___ \ ) _)  )   /
+echo 		(_/\_)(__)\_)__)(__\_)\_/\_/(__\_)  \____/(__)\___)(____)\_)__)(____/(____)(__\_)
+echo.
+echo.
+echo 		Successfully removed the license!
+	
+@ping -n 5 localhost> nul
+goto gui
 
-bitsadmin /transfer "Download Key" /download /priority normal https://raw.githubusercontent.com/InvalidPandaa/winrar-licenser/main/WinRAR%20Licenser/rarreg.key "C:\Program Files\WinRAR\rarreg.key"
 
-
-pause
+:exit
+exit
